d8180fba72b8d347aabb18ac43cdf9e2c76c5341
Asif Alam <alamtheboss232@gmail.com>
2022-08-20 15:49:18 +0100

    implement stars and numbers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+const PlayNumber = (props) => (
+    <button className={"number"}>{props.number}</button>
+);
+
+const StarMatch = () => {
+    const [stars, setStars] = useState(utils.random(1, 9));
+    return (
+        <div className="game">
+            <div className="help">
+                Pick 1 or more numbers that sum to the number of stars
+            </div>
+            <div className="body">
+                <div className="left">
+                    {utils.range(1, stars).map(starId =>
+                        <div key={starId} className={"star"}></div>
+                    )}
+                </div>
+                <div className="right">
+                    {utils.range(1, 9).map(number =>
+                        <PlayNumber key={number} number={number}/>
+                    )}
+                </div>
+            </div>
+            <div className="timer">Time Remaining: 10</div>
+        </div>
+    );
+};
+
+// Color Theme
+const colors = {
+    available: 'lightgray',
+    used: 'lightgreen',
+    wrong: 'lightcoral',
+    candidate: 'deepskyblue',
+};
+
+// Math science
+const utils = {
+    // Sum an array
+    sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+    // create an array of numbers between min and max (edges included)
+    range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+    // pick a random number between min and max (edges included)
+    random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+    // Given an array of numbers and a max...
+    // Pick a random sum (< max) from the set of all available sums in arr
+    randomSumIn: (arr, max) => {
+        const sets = [[]];
+        const sums = [];
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0, len = sets.length; j < len; j++) {
+                const candidateSet = sets[j].concat(arr[i]);
+                const candidateSum = utils.sum(candidateSet);
+                if (candidateSum <= max) {
+                    sets.push(candidateSet);
+                    sums.push(candidateSum);
+                }
+            }
+        }
+        return sums[utils.random(0, sums.length - 1)];
+    },
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <>
+          <StarMatch />
+      </>
+    {/*s*/}
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
